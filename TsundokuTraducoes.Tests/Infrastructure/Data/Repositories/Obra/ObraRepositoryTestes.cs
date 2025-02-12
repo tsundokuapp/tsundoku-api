69fc82816eb3f22ce5f6b92d87e7c4fa0d2f34f1
Edson Ramos <edson.ramos@gmail.com>
2025-02-12 09:07:38 -0300

    feat: adicionados testes comics e ajustes generos comics

--- TsundokuTraducoes.Tests/Infrastructure/Data/Repositories/Obra/ObraRepositoryTestes.cs
+++ TsundokuTraducoes.Tests/Infrastructure/Data/Repositories/Obra/ObraRepositoryTestes.cs
@@ -18,6 +18,8 @@ namespace TsundokuTraducoes.Tests.Infrastructure.Data.Repositories.Obra
                 .Options;            
         }
 
+        #region => TESTES REPOSITORY - NOVEL        
+
         [Fact]
         public async Task RepositoryObra_DeveCadastrarUmaNovel()
         {
@@ -246,10 +248,249 @@ namespace TsundokuTraducoes.Tests.Infrastructure.Data.Repositories.Obra
             await Dispose();
         }
 
+        #endregion
+
+        #region => TESTES REPOSITORY - COMIC        
+
+        [Fact]
+        public async Task RepositoryObra_DeveCadastrarUmaComic()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var novel = obraRepositoryFactory.GerarComic();
+            novel.Id = id;
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(novel);
+                var retorno = await repository.AlteracoesSalvas();
+                var resultado = repository.RetornaComicPorId(id);
+
+                // Assert
+                Assert.True(retorno);
+                Assert.NotNull(resultado);
+                Assert.Equal("Hatsukoi Losstime", resultado.Titulo);
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveAtualizarUmaComic()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(comic);
+                var retornoObraSalva = await repository.AlteracoesSalvas();
+                var resultadoObraCadastrada = repository.RetornaComicPorId(id);
+
+                var obraDTO = new ObraDTO { Id = id, Alias = "Tristeza do Bunas" };
+                var obraAtualizada = repository.AtualizaComic(obraDTO);
+
+                // Assert
+                Assert.True(retornoObraSalva);
+                Assert.NotNull(resultadoObraCadastrada);
+                Assert.Equal(resultadoObraCadastrada.Id, obraAtualizada.Id);
+                Assert.Equal("Tristeza do Bunas", obraAtualizada.Alias);
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveRetornarUmaListaDeComics()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(comic);
+                var retornoObraSalva = await repository.AlteracoesSalvas();
+                var resultadoObraCadastrada = repository.RetornaComicPorId(id);
+                var retorno = repository.RetornaListaComics();
+
+
+                // Assert
+                Assert.True(retornoObraSalva);
+                Assert.NotNull(resultadoObraCadastrada);
+                Assert.True(retorno.Count > 0);
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveRetornarUmaComicPorSlugValido()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+            var slug = "hatsukoi-losstime";
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(comic);
+                var retornoObraSalva = await repository.AlteracoesSalvas();
+                var resultadoObraCadastrada = repository.RetornaComicPorId(id);
+                var retorno = repository.RetornaComicPorSlug(slug);
+
+                // Assert
+                Assert.True(retornoObraSalva);
+                Assert.NotNull(resultadoObraCadastrada);
+                Assert.NotNull(retorno);
+                Assert.Equal(slug, retorno.Slug);
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveExcluirUmaComic()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                context.Comics.Add(comic);
+                context.SaveChanges();
+
+                repository.ExcluiComic(comic);
+                var obraExcluida = await repository.AlteracoesSalvas();
+
+                var resultadoObraExcluida = repository.RetornaComicPorId(comic.Id);
+
+                // Assert
+                Assert.True(obraExcluida);
+                Assert.Null(resultadoObraExcluida);
+                Assert.Equal(0, context.Comics.Count());
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveRetornarUmaComicExistente()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+            var titulo = "Hatsukoi Losstime";
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(comic);
+                var retornoObraSalva = await repository.AlteracoesSalvas();
+                var resultadoObraExistente = repository.RetornaComicExistente(titulo);
+
+                // Assert
+                Assert.True(retornoObraSalva);
+                Assert.NotNull(resultadoObraExistente);
+                Assert.Equal(titulo, resultadoObraExistente.Titulo);
+            }
+
+            await Dispose();
+        }
+
+        [Fact]
+        public async Task RepositoryObra_DeveInserirUmaListaDeGenerosNaComic()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var obraRepositoryFactory = new ObraRepositoryFactory();
+            var comic = obraRepositoryFactory.GerarComic();
+            comic.Id = id;
+            var listaGenero = new List<string> { "Romance", "Vida Escolar", "Seinen" };
+
+            await using (var context = new ContextBase(_options))
+            {
+                IObraRepository repository = new ObraRepository
+                    (context,
+                    new GeneroDeParaRepository(context),
+                    new GeneroRepository(context));
+
+                // Act
+                await repository.AdicionaComic(comic);
+                var retornoObraSalva = await repository.AlteracoesSalvas();
+
+                await repository.InsereGenerosComic(comic, listaGenero, true);
+
+                var retornoObraComListaGenero = repository.RetornaComicPorId(comic.Id);
+
+                // Assert
+                Assert.True(retornoObraSalva);
+                Assert.NotNull(retornoObraComListaGenero);
+                Assert.True(retornoObraComListaGenero.GenerosComic.Count == listaGenero.Count);
+            }
+
+            await Dispose();
+        }
+
+        #endregion
+
+        #region => DISPOSE
+
         private async Task Dispose()
         {
             await using var context = new ContextBase(_options);
             await context.Database.EnsureDeletedAsync();
         }
+
+        #endregion
+
     }
 }
