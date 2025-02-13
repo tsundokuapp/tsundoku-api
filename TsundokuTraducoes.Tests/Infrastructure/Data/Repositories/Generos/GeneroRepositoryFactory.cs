88e17464189e352920b13782626e268c276c9a97
Edson Ramos <edson.ramos@gmail.com>
2025-02-13 07:04:03 -0300

    feat: add regions para separar

--- TsundokuTraducoes.Tests/Infrastructure/Data/Repositories/Generos/GeneroRepositoryFactory.cs
+++ TsundokuTraducoes.Tests/Infrastructure/Data/Repositories/Generos/GeneroRepositoryFactory.cs
@@ -6,6 +6,8 @@ namespace TsundokuTraducoes.Tests.Infrastructure.Data.Repositories.Generos
 {
     public class GeneroRepositoryFactory
     {
+        #region => MOCK OBRAS
+
         public Novel GerarNovel()
         {
             return new Novel() { Id = Guid.NewGuid() };
@@ -16,19 +18,33 @@ namespace TsundokuTraducoes.Tests.Infrastructure.Data.Repositories.Generos
             return new Comic() { Id = Guid.NewGuid() };
         }
 
+        #endregion
+                
+        #region => MOCK GENERO
+        
         public Genero GerarGenero(string descricao, string slug)
         {
             return new Genero { Id = Guid.NewGuid(), Descricao = descricao, Slug = slug, DataInclusao = DateTime.Now, DataAlteracao = DateTime.Now };
         }
 
+        #endregion
+
+        #region => MOCK GENERO NOVEL
+        
         public GeneroNovel GerarGeneroNovel(Guid idObra, Guid idGenero)
         {
             return new GeneroNovel() { NovelId = idObra, GeneroId = idGenero };
-        }
+        }        
 
+        #endregion
+
+        #region => MOCK GENERO COMIC
+        
         public GeneroComic GerarGeneroComic(Guid idObra, Guid idGenero)
         {
             return new GeneroComic() { ComicId = idObra, GeneroId = idGenero };
         }
+        
+        #endregion
     }
 }
