6ff7da0b5d1111c9d00a3158d00d74a760092795
Edson Ramos <edson.ramos@gmail.com>
2025-03-28 22:17:59 -0300

    feat: adição de testes generos

---
+++ TsundokuTraducoes.Tests/Services/AppServices/Generos/GeneroAppServiceFactoryTestes.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using TsundokuTraducoes.Entities.Entities.Generos;
+using TsundokuTraducoes.Tests.Utils;
+
+namespace TsundokuTraducoes.Tests.Services.AppServices.Generos
+{
+    public class GeneroAppServiceFactoryTestes
+    {
+        public Genero GerarGenero(string descricaoGenero, string slugGenero)
+        {
+            return FixtureCustomizado.RetornaFixtureCustomizado
+                .Build<Genero>()
+                .With(x => x.Descricao, descricaoGenero)
+                .With(x => x.Slug, slugGenero)
+                .Create();
+        }
+
+        public List<Genero> GerarListaGeneros(Dictionary<string, string> dicionarioDescricaoGenero)
+        {
+            var listaGenero = new List<Genero>();
+
+            foreach (var genero in dicionarioDescricaoGenero)
+            {
+                listaGenero.Add(GerarGenero(genero.Key, genero.Value));
+            }
+
+            return listaGenero;
+        }
+    }
+}
