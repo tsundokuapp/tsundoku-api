8ff8c768eed8aa44ae9e941303b048ce760a6244
Edson Ramos <edson.ramos@gmail.com>
2025-06-21 20:02:16 -0300

    fix: correção rota capitulos comics

--- TsundokuTraducoes/Controllers/Publico/comic/CapitulosComicsController.cs
+++ TsundokuTraducoes/Controllers/Publico/comic/CapitulosComicsController.cs
@@ -10,7 +10,7 @@ namespace TsundokuTraducoes.Api.Controllers.Publico.manga
     [ApiController]
     public class CapitulosComicsController(ICapituloComicAppService service) : Controller, IBaseService<ICapituloComicAppService>
     {
-        [HttpGet("api/comics/{idObra}/{idCapitulo}")]
+        [HttpGet("api/capitulos/comics/{idObra}/{idCapitulo}")]
         public async Task<IActionResult> ObterCapitulosComicPorIdObraEIdCapitulo(Guid idObra, Guid idCapitulo)
         {
             var result = await service.ObterCapitulosComicPorIdObraEIdCapitulo(idObra, idCapitulo);
@@ -21,7 +21,7 @@ namespace TsundokuTraducoes.Api.Controllers.Publico.manga
             return Ok(objetoRetorno);
         }
 
-        [HttpGet("api/comics/slug/{slugObra}/{idCapitulo}")]
+        [HttpGet("api/capitulos/comics/slug/{slugObra}/{idCapitulo}")]
         public async Task<IActionResult> ObterCapitulosComicPorSlugObraEIdCapitulo(string slugObra, Guid idCapitulo)
         {
             var result = await service.ObterCapitulosComicPorSlugObraEIdCapitulo(slugObra, idCapitulo);
@@ -32,7 +32,7 @@ namespace TsundokuTraducoes.Api.Controllers.Publico.manga
             return Ok(objetoRetorno);
         }
 
-        [HttpGet("api/comics/slug/{slugObra}")]
+        [HttpGet("api/capitulos/comics/slug/{slugObra}")]
         public async Task<IActionResult> ObterListaCapitulosComicPorSlugObra(string slugObra, [FromQuery] int? skip, int? take)
         {
             var result = await service.ObterListaCapitulosComicPorSlugObra(slugObra);
